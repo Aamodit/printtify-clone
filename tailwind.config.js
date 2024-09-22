8090580fb7b159600c1723a4c5cfd71ee58915f4
Aamodit <aamoditsaini@gmail.com>
2024-09-22 20:07:18 +0530

    push3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
-// tailwind.config.js
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // make sure this path is correct
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
